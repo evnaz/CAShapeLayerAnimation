dcb7daf2b677fdb29d8f5ec61673113d4df3b1e1
Evgeny <e.nazarov@yahoo.com>
2015-07-13 21:38:45 +0500

    Initial Commit

---
+++ BezierPath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BezierPath
+//
+//  Created by Evgeny on 13.07.15.
+//  Copyright (c) 2015 Evgeny Nazarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BezierPath/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BezierPath
+//
+//  Created by Evgeny on 13.07.15.
+//  Copyright (c) 2015 Evgeny Nazarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
